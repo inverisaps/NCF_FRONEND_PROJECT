60a067d1690b4060d0961afb1a390919e447bf8f
elianmartinez157@gmail.com <inverisaps@gmail.com>
2021-03-08 11:39:30 -0400

    Disenio completo, sin javascript

---
+++ js/funtion.js
@@ -0,0 +1,6 @@
+var rnc_input = document.querySelector('#rnc-input');
+
+var nombre_input =  document.querySelector('#nombre-input');
+
+var monto_input = document.querySelector('#monto_input');
+
--- js/responsive.js
+++ js/responsive.js
@@ -2,8 +2,11 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
   let heightKeyboard = screen.height * 0.5;
   let teclado = document.getElementById('teclado'); 
-
   teclado.style.height = heightKeyboard + "px";
-
- 
   });
+
+
+
+  function klk(){
+   scroll(0,150);
+  }
